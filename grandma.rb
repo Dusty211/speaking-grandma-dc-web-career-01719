112ff835d9419f09f1360aad4d0ff1af4be7368f
Kyle Houghton <kylebenz@hotmail.com>
2018-12-18 21:52:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,12 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(user_input)
-  if user_input == user_input.upcase
-    return "NO, NOT SINCE 1938!"
-  elsif user_input == "I LOVE YOU GRANDMA!"
+  if user_input == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif user_input == user_input.upcase
+    return "NO, NOT SINCE 1938!"
   else
     return "HUH? SPEAK UP, SONNY!"
   end
 end
-
